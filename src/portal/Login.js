c705c93668609f919fa0598786e7a5f90bf855bd
actuallydan <dan_kral@yahoo.com>
2018-11-07 23:53:50 -0500

    fix(login): partially fixed issue where sometimes user wouldnt be able to reattempt login if incorrect initially

--- src/portal/Login.js
+++ src/portal/Login.js
@@ -1,24 +1,30 @@
-import React, { Fragment, Component } from "react";
-import FeatherIcon from "feather-icons-react";
-import swal from "sweetalert";
-import { Link, withRouter } from "react-router-dom";
-import { withGun } from "react-gun";
-import { updateUser, updatePub, updateChannel, updateCircle, updateRevision } from "../store/state/actions";
-import { validateLogin } from "../utils/validators";
-import { pull } from "../store/state/reducers";
-import { connect } from "react-redux";
+import React, { Fragment, Component } from 'react';
+import FeatherIcon from 'feather-icons-react';
+import swal from 'sweetalert';
+import { Link, withRouter } from 'react-router-dom';
+import { withGun } from 'react-gun';
+import {
+    updateUser,
+    updatePub,
+    updateChannel,
+    updateCircle,
+    updateRevision
+} from '../store/state/actions';
+import { validateLogin } from '../utils/validators';
+import { pull } from '../store/state/reducers';
+import { connect } from 'react-redux';
 
 class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            password: "",
-            email: ""
+            password: '',
+            email: ''
         };
     }
     componentDidMount() {
         if (this.props.user) {
-            this.props.history.push("/app");
+            this.props.history.push('/app');
         } else {
             this.props.dispatch(updateChannel(null));
             this.props.dispatch(updateCircle(null));
@@ -30,7 +36,7 @@ class Login extends Component {
         const isValid = validateLogin({ ...this.state });
 
         if (isValid !== undefined) {
-            swal("Error", isValid[Object.keys(isValid)[0]][0], "error");
+            swal('Error', isValid[Object.keys(isValid)[0]][0], 'error');
             return false;
         }
 
@@ -38,106 +44,108 @@ class Login extends Component {
 
         let newUser = this.props.gun.user();
 
-        newUser.auth(email, password, ack => {
+        newUser.auth(email, password, async ack => {
             if (ack.err) {
                 console.log(ack.err);
-                if (
-                    ack.err ===
-                    "Auth attempt failed! Reason: Failed to decrypt secret! 0/1"
-                ) {
-                    swal("Error", "Invalid password", "error");
+                if (ack.err.indexOf('Auth attempt failed!')) {
+                    swal('Error', 'Incorrect password', 'error');
                 } else {
                     swal(
-                        "Error",
-                        "No user found with that information.",
-                        "error"
+                        'Error',
+                        'No user found with that information.',
+                        'error'
                     );
                 }
+                // Prevent user from being able to re-attempt login
+                // newUser.leave();
+                // above doesn't work https://github.com/amark/gun/issues/468
+                await this.setState({
+                    password: ''
+                });
+                newUser = null;
                 return false;
             }
-            newUser.get("profile").once(profile => {
+            newUser.get('profile').once(profile => {
                 // set the public key and id in redux to log in
                 this.props.dispatch(updateUser(profile.id));
                 this.props.dispatch(updatePub(ack.pub));
                 // start listening to changes on our user
                 this.props.listen();
-                this.props.history.push("/app");
+                this.props.history.push('/app');
             });
         });
     };
     updateInfo = () => {
         this.setState({
-            password: document.getElementById("loginPassword").value,
-            email: document.getElementById("loginEmail").value
+            password: document.getElementById('loginPassword').value,
+            email: document.getElementById('loginEmail').value
         });
     };
-    shouldComponentUpdate(nextProps, nextState){
+    shouldComponentUpdate(nextProps, nextState) {
         return nextState !== this.state;
     }
     render() {
         const { email, password } = this.state;
         return (
             <Fragment>
-                <div id="portal-header">
+                <div id='portal-header'>
                     <img
-                        src="/img/Athares-logo-small-white.png"
-                        id="portal-logo"
-                        alt="logo"
+                        src='/img/Athares-logo-small-white.png'
+                        id='portal-logo'
+                        alt='logo'
                     />
                     <img
-                        src="/img/Athares-full-small-white.png"
-                        id="portal-brand"
-                        alt="brand"
+                        src='/img/Athares-full-small-white.png'
+                        id='portal-brand'
+                        alt='brand'
                     />
                 </div>
                 <form
-                    className="wrapper"
-                    id="portal-login"
-                    onSubmit={this.tryLogin}
-                >
-                    <p className="portal-text">Login with the form below</p>
-                    <div className="portal-input-wrapper">
+                    className='wrapper'
+                    id='portal-login'
+                    onSubmit={this.tryLogin}>
+                    <p className='portal-text'>Login with the form below</p>
+                    <div className='portal-input-wrapper'>
                         <FeatherIcon
-                            className="portal-input-icon h1 w1"
-                            icon="at-sign"
+                            className='portal-input-icon h1 w1'
+                            icon='at-sign'
                         />
                         <input
-                            placeholder="Email"
-                            className="portal-input h2 ghost pa2"
+                            placeholder='Email'
+                            className='portal-input h2 ghost pa2'
                             required
-                            type="email"
+                            type='email'
                             onChange={this.updateInfo}
                             value={email}
-                            id="loginEmail"
-                            tabIndex="1"
+                            id='loginEmail'
+                            tabIndex='1'
                         />
                     </div>
-                    <div className="portal-input-wrapper">
+                    <div className='portal-input-wrapper'>
                         <FeatherIcon
-                            className="portal-input-icon h1 w1"
-                            icon="lock"
+                            className='portal-input-icon h1 w1'
+                            icon='lock'
                         />
                         <input
-                            type="password"
-                            className="portal-input h2 ghost pa2"
-                            placeholder="Password"
-                            id="loginPassword"
+                            type='password'
+                            className='portal-input h2 ghost pa2'
+                            placeholder='Password'
+                            id='loginPassword'
                             onChange={this.updateInfo}
                             value={password}
-                            tabIndex="2"
+                            tabIndex='2'
                         />
                     </div>
                     <button
-                        id="login-button"
-                        className="f6 link dim br-pill ba bg-white bw1 ph3 pv2 mb2 dib black"
+                        id='login-button'
+                        className='f6 link dim br-pill ba bg-white bw1 ph3 pv2 mb2 dib black'
                         onClick={this.tryLogin}
-                        tabIndex="3"
-                    >
+                        tabIndex='3'>
                         LOGIN
                     </button>
-                    <Link to="register">
-                        {" "}
-                        <div className="switch-portal">I want to register</div>
+                    <Link to='register'>
+                        {' '}
+                        <div className='switch-portal'>I want to register</div>
                     </Link>
                 </form>
             </Fragment>
@@ -147,7 +155,7 @@ class Login extends Component {
 
 function mapStateToProps(state) {
     return {
-        user: pull(state, "user")
+        user: pull(state, 'user')
     };
 }
 export default connect(mapStateToProps)(withRouter(withGun(Login)));
