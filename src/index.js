b390ee42b6784463707c93bb1e98b37bc32f1629
actuallyDan <dankral01@gmail.com>
2018-12-31 13:33:05 -0500

    feat(graphql): begin reimplementing apollo-client graphql

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import * as reducers from './store/reducers';
-import { loadingBarReducer } from 'react-redux-loading-bar';
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import * as reducers from "./store/reducers";
+import { loadingBarReducer } from "react-redux-loading-bar";
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+import { ApolloProvider } from "react-apollo";
+// https://www.apollographql.com/docs/react/essentials/get-started.html
+const client = new ApolloClient({
+  uri: "https://api.graph.cool/simple/v1/cjfkj77t63cgt01543zhvxfo6"
+});
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-    combineReducers({ ...reducers, loadingBar: loadingBarReducer }),
-    composeEnhancers(applyMiddleware(thunk))
+  combineReducers({ ...reducers, loadingBar: loadingBarReducer }),
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
