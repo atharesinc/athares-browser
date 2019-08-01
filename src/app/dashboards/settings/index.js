e56c446f483484274a15811de259ae15a0eef3cf
Daniel Kral <dkral@mobiquityinc.com>
2019-08-01 16:43:43 -0400

    fix(settings): fixed path issue with settings components

--- src/app/dashboards/settings/index.js
+++ src/app/dashboards/settings/index.js
@@ -1,25 +1,25 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { pull } from "../../../store/state/reducers";
-import { updateCircle } from "../../../store/state/actions";
-import swal from "sweetalert";
-import FeatherIcon from "feather-icons-react";
-import { Link } from "react-router-dom";
-import LeaveCircle from "./LeaveCircle";
-import ShareCircle from "./ShareCircle";
-import CirclePrefs from "./CirclePrefs";
-import ScrollBars from "react-custom-scrollbars";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { pull } from '../../../store/state/reducers';
+import { updateCircle } from '../../../store/state/actions';
+import swal from 'sweetalert';
+import FeatherIcon from 'feather-icons-react';
+import { Link } from 'react-router-dom';
+import LeaveCircle from './LeaveCircle';
+import ShareCircle from './ShareCircle';
+import CirclePrefs from './CirclePrefs';
+import ScrollBars from 'react-custom-scrollbars';
 
 class Settings extends Component {
   componentDidMount() {
     // verify this circle is real and that the user is logged in, but for now...
     if (!this.props.user || !this.props.activeCircle) {
-      this.props.history.replace("/app");
+      this.props.history.replace('/app');
     }
   }
   componentDidUpdate() {
     if (!this.props.user || !this.props.activeCircle) {
-      this.props.history.replace("/app");
+      this.props.history.replace('/app');
     }
   }
   leaveCircle = e => {
@@ -28,21 +28,21 @@ class Settings extends Component {
 
     swal("Are you sure you'd like to leave this Circle?", {
       buttons: {
-        cancel: "Not yet",
-        confirm: true
-      }
+        cancel: 'Not yet',
+        confirm: true,
+      },
     }).then(async value => {
       if (value === true) {
         this.props.deleteUserFomCircle({
           variables: {
             user,
-            circle: activeCircle
-          }
+            circle: activeCircle,
+          },
         });
         swal(
-          "Removed From Circle",
+          'Removed From Circle',
           `You have left this Circle. You will have to be re-invited to participate at a later time.`,
-          "warning"
+          'warning',
         );
         this.props.dispatch(updateCircle(null));
         this.props.history.push(`/app`);
@@ -55,23 +55,23 @@ class Settings extends Component {
   render() {
     let { user, activeCircle } = this.props;
     return (
-      <div id="revisions-wrapper">
-        <div className="flex ph2 mobile-nav">
-          <Link to="/app" className="flex justify-center items-center">
+      <div id='revisions-wrapper'>
+        <div className='flex ph2 mobile-nav'>
+          <Link to='/app' className='flex justify-center items-center'>
             <FeatherIcon
-              icon="chevron-left"
-              className="white db dn-l"
+              icon='chevron-left'
+              className='white db dn-l'
               onClick={this.back}
             />
           </Link>
-          <h2 className="ma3 lh-title white"> Settings </h2>
+          <h2 className='ma3 lh-title white'> Settings </h2>
         </div>
         <div
-          id="create-circle-form"
-          className="pa2 pa4-ns white wrapper mobile-body"
+          id='create-circle-form'
+          className='pa2 pa4-ns white wrapper mobile-body'
         >
           <ScrollBars
-            style={{ width: "100%", height: "100%" }}
+            style={{ width: '100%', height: '100%' }}
             autoHide
             autoHideTimeout={1000}
             autoHideDuration={200}
@@ -89,8 +89,8 @@ class Settings extends Component {
 
 function mapStateToProps(state) {
   return {
-    user: pull(state, "user"),
-    activeCircle: pull(state, "activeCircle")
+    user: pull(state, 'user'),
+    activeCircle: pull(state, 'activeCircle'),
   };
 }
 
